4900979703fb035d9c1045dcf2a075bbf4b9d775
keiikegami <sybaster.x@gmail.com>
2017-06-19 15:09:35 +0900

    no message

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using DefferedAcceptance
 using Base.Test
 
-const _deferred_acceptance = ikegami
+
+
+const _deferred_acceptance = DefferedAcceptance.ikegami
 
 function mat2vecs{T<:Integer}(prefs::Matrix{T})
     return [prefs[1:findfirst(prefs[:, j], 0)-1, j] for j in 1:size(prefs, 2)]
